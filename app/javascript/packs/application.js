a14fe9f9a74a0822735ef50b1d36172a93d1acd7
sahilmarka <m.sahilkumar1234@gmail.com>
2021-05-26 21:05:54 +0530

    add: UI styling

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+require("bootstrap/dist/js/bootstrap")
