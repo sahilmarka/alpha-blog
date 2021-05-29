26afcb0f0d189ece057b3baba5d6478e6347ed76
sahilmarka <m.sahilkumar1234@gmail.com>
2021-05-29 19:54:05 +0530

    make user to edit his credentials and able to signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
+  has_many :articles
   validates :username, presence: true, 
                        uniqueness:{case_sensitive: false}, 
                        length: {minimum: 3, maximum: 25}
@@ -7,5 +8,6 @@ class User < ApplicationRecord
                      uniqueness:{case_sensitive: false},
                      length: { maximum: 105},
                      format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
-    has_many :articles
+    
+    has_secure_password
 end
